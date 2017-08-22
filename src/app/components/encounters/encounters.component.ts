30bc3501b7530753420018a01fedcc9ea964608f
David Hilscher <dhilscher@Davids-MacBook-Air.local>
2017-08-22 16:22:11 -0700

    post encounters moved to report

--- src/app/components/encounters/encounters.component.ts
+++ src/app/components/encounters/encounters.component.ts
@@ -16,20 +16,6 @@ export class EncountersComponent implements OnInit {
 async ngOnInit() {
   const encounters = await this.encounterService.getReport();
   console.log(encounters);
-
-
-
-
-
-  const data = {
-    date: '2011-10-10',
-    colonist_id: '3',
-    atype: 'spider',
-    action: 'ham'
-  }
-  const newEncounters = await this.encounterService.reportEncounter(data);
-  console.log(newEncounters);
-  
   }
 
 }
--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AlienService } from '../../services/alien';
+import { EncounterService } from '../../services/encounters';
+
 
 
 @Component({
@@ -8,15 +10,28 @@ import { AlienService } from '../../services/alien';
   styles: [],
   providers: [
     AlienService,
+    EncounterService
   ]
 })
 export class ReportComponent implements OnInit {
 
-  constructor(private alienService: AlienService) { }
+  constructor(
+    private alienService: AlienService,
+    private encounterService: EncounterService,
+    ) { }
 
   async ngOnInit() {
     const aliens = await this.alienService.getAliens();
     console.log(aliens);
+
+    const data = {
+    date: '2011-10-10',
+    colonist_id: '3',
+    atype: 'spider',
+    action: 'ham'
+    }
+  const newEncounters = await this.encounterService.reportEncounter(data);
+  console.log(newEncounters);
   }
 
 }
