09e66178a920e9aa36fd287953bc506798b3b3bc
David Hilscher <dhilscher@Davids-MacBook-Air.local>
2017-08-23 11:29:20 -0700

    circle pulse added to home

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ColonistService } from '../../services/colonist';
 import { JobService } from '../../services/job';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -12,24 +14,14 @@ import { JobService } from '../../services/job';
   ]
 })
 export class RegisterComponent implements OnInit {
-
+  
   constructor(
     private jobService: JobService,
     private colonistService: ColonistService,
     ) { }
 
   async ngOnInit() {
-    const data = {
-      name: 'Hello there',
-      age: '50',
-      job_id: '2'
-    };
     
-    const newColonist = await this.colonistService.registerColonist(data);
-    console.log(newColonist);
-
-    const jobs = await this.jobService.getJobs();
-    console.log(jobs);
   }
 
 }
