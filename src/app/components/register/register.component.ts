460f7776682f3dca7771560453b6fd9f57469561
David Hilscher <dhilscher@Davids-MacBook-Air.local>
2017-08-23 14:08:41 -0700

    new colonist form posting

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ColonistService } from '../../services/colonist';
 import { JobService } from '../../services/job';
 import { Job } from '../../models/job';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators, ValidatorFn } from '@angular/forms';
+import { NewColonist } from '../../models/colonist'
 
 
 @Component({
@@ -14,13 +15,13 @@ import { FormControl, FormGroup } from '@angular/forms';
     ColonistService
   ]
 })
-export class RegisterComponent implements OnInit {
+  export class RegisterComponent implements OnInit {
   jobs: Job[];
 
   registerForm = new FormGroup({
-    name: new FormControl(''),
-    age: new FormControl(''),
-    job_id: new FormControl('')
+    name: new FormControl('', [Validators.required]),
+    age: new FormControl('', [Validators.required]),
+    job_id: new FormControl('', [Validators.required])
   });
   
   constructor(
@@ -30,5 +31,15 @@ export class RegisterComponent implements OnInit {
 
   async ngOnInit() {
   this.jobs = await this.jobService.getJobs();    
+  }
+  async registerColonist() {
+    const newColonist: NewColonist = {
+      name: this.registerForm.get('name').value,
+      age: this.registerForm.get('age').value,
+      job_id: this.registerForm.get('job_id').value
+    };
+
+    const colonist = await this.colonistService.registerColonist(newColonist);
+    console.log('colonist was saved!', colonist);
   }
 }
